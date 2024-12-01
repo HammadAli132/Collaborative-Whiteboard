934fec430b09e66d2c3bf09b67c56eff283ec014
HammadAli132 <108983041+HammadAli132@users.noreply.github.com>
2024-12-01 22:15:33 +0500

    Project Completed

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,8 @@ const { addMessage, getMessages } = require('./utils/messages.js');
 
 const express = require("express")
 const http = require("http")
-const { Server } = require("socket.io")
+const { Server } = require("socket.io");
+const path = require('path');
 
 const app = express()
 const server = http.createServer(app)
@@ -12,6 +13,8 @@ const io = new Server(server, {
         origin: process.env.MODE === 'development' ? 'http://localhost:3000' : process.env.CLIENT_URL
     }
 })
+
+__dirname = path.resolve()
 
 const validRoomIDs = new Set(); // Maintain valid room IDs
 
@@ -93,6 +96,13 @@ io.on('connection', (socket) => {
 })
 
 const PORT = process.env.PORT || 5000
+
+if (process.env.MODE !== 'development') {
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"))
+    })
+}
 
 server.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`)
